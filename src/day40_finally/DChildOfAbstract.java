c8315c66aef76f6f9642e37b69cf5e856cb7fcd9
tarikklckya <109520611+tarikklckya@users.noreply.github.com>
2022-08-05 23:19:14 +0300

    05.08.2022

--- src/day40_finally/DChildOfAbstract.java
+++ src/day40_finally/DChildOfAbstract.java
@@ -5,6 +5,13 @@ public class DChildOfAbstract extends CAbstract{
     @Override
     void carpma() {
 
+        /*
+        Abstract classlar child classların
+        sahip olmaları gereken mecburi özellikleri belirlerler.
+        (günlük hayatımızdaki standartlar gibi)
+        Abstract bir classı parent edinen tğm childlar
+        parent abstract classtaki tüm abstract methodları override etmek zorundadırlar.
+         */
     }
 
     @Override
