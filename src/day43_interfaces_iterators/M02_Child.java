4e70501efee575d4a53ced53dfa90a3d3dad2fcb
tarikklckya <109520611+tarikklckya@users.noreply.github.com>
2022-08-08 23:25:15 +0300

    08.08.2022

--- src/day43_interfaces_iterators/M02_Child.java
+++ src/day43_interfaces_iterators/M02_Child.java
@@ -1,6 +1,6 @@
 package day43_interfaces_iterators;
 
-public class C02_Child implements I01_InterfaceBodyOlanMethod {
+public class M02_Child implements I01_InterfaceBodyOlanMethod {
 
     @Override
     public void motor() {
@@ -26,7 +26,7 @@ public class C02_Child implements I01_InterfaceBodyOlanMethod {
     public static void main(String[] args) {
         I01_InterfaceBodyOlanMethod.direksiyon();
 
-        C02_Child obj = new C02_Child();
+        M02_Child obj = new M02_Child();
         obj.teker();
     }
 }
